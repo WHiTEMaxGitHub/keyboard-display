702baad57791ea4531019ac3838721b1f9ad8741
WHiTEMaxGitHub <microwhite233@outlook.com>
2026-07-09 17:36:17 +0800

    tauri-init
    Change-Id: I7f841efa203c9644468d94b9514f93bd44023786

---
+++ src-tauri/gen/android/app/src/main/java/com/whitemax/keyword_display/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.whitemax.keyword_display
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
